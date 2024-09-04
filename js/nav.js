1236e915558d9b1b9951c55ab668e4f2c570a83c
zBION1C <nmontana745@gmail.com>
2024-09-04 13:43:55 +0200

    trying css

--- js/nav.js
+++ js/nav.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
     $("#nav-placeholder").load("/static/nav.html");
+    $('nav li a').filter(function () {
+      return this.href === location.href;
+  }).addClass('active');
 });
