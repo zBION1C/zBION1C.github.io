b719298643d61ae07616584f6412f04385d6ddb0
zBION1C <nmontana745@gmail.com>
2024-09-04 12:26:14 +0200

    trying css

--- js/nav.js
+++ js/nav.js
@@ -1,22 +1,3 @@
-$(document).ready(function() {
-    // Load the navigation bar into the placeholder div
-    $("#navbar-placeholder").load("/static/nav.html", function() {
-        // Get the current URL path
-        var currentPath = window.location.pathname;
-
-        // Check if the path matches the Posts page
-        if (currentPath.includes("/static/posts/posts.html")) {
-            // Remove 'active' class from any currently active element
-            $("nav a").removeClass("active");
-
-            // Add 'active' class to the Posts link
-            $("#post").addClass("active");
-        } else if (currentPath.includes("/index.html")) {
-            // For home page, ensure home is active
-            $("nav a").removeClass("active");
-            $("#home").addClass("active");
-        }
-
-        // Add more conditions if needed for other pages
-    });
+$(function(){
+  $("#nav-placeholder").load("/static/nav.html");
 });
