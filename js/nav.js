218c2142f012a61eee968d80d576b66cf6c1991e
zBION1C <nmontana745@gmail.com>
2024-09-04 14:09:59 +0200

    trying css

--- js/nav.js
+++ js/nav.js
@@ -1,21 +1,3 @@
 $(document).ready(function() {
     $("#nav-placeholder").load("/static/nav.html");
-    var links = $('#nav-placeholder a');
-    console.log(links)
-    links.each(function() {
-        // Get the href attribute of each <a>
-        var href = $(this).attr('href');
-        links.each(function() {
-            console.log($(this).attr('href'));
-        });
-
-        // Check if the href matches the current path
-        if (currentPath === href) {
-            // Remove 'active' class from all <a> tags
-            $('#navbar-placeholder nav a').removeClass('active');
-
-            // Add 'active' class to the matching <a>
-            $(this).addClass('active');
-        }
-    });
 });
