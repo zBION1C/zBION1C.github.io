1544a69e0fbbec8ebbeb41d67811877555525f22
zBION1C <nmontana745@gmail.com>
2024-09-04 12:41:14 +0200

    trying css

--- js/nav.js
+++ js/nav.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
-  // Load the navigation bar into the placeholder div
-  $("#navbar-placeholder").load("/static/nav.html", function() {
-      // Remove 'active' class from any currently active element
-      $("#navbar-placeholder nav a").removeClass("active");
+    // Load the navigation bar into the placeholder div
+    $("#navbar-placeholder").load("/static/nav.html", function() {
+        // Remove 'active' class from any currently active element
+        $("nav a").removeClass("active");
 
-      // Get the current URL path
-      var currentPath = window.location.pathname;
+        // Get the current URL path
+        var currentPath = window.location.pathname;
 
-      // Highlight based on the current path
-      if (currentPath.includes("/static/posts/posts.html")) {
-          // Add 'active' class to the Posts link
-          $("#navbar-placeholder #post").addClass("active");
-      } else if (currentPath.includes("/index.html")) {
-          // Add 'active' class to the Home link
-          $("#navbar-placeholder #home").addClass("active");
-      }
+        // Highlight based on the current path
+        if (currentPath.includes("/static/posts/posts.html")) {
+            // Add 'active' class to the Posts link
+            $("#post").addClass("active");
+        } else if (currentPath.includes("/index.html")) {
+            // Add 'active' class to the Home link
+            $("#home").addClass("active");
+        }
 
-      // Add more conditions if needed for other pages
-  });
+        // Add more conditions if needed for other pages
+    });
 });
