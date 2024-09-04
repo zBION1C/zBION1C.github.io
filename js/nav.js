d46193babdd5894e66d799068585407e537d128a
zBION1C <nmontana745@gmail.com>
2024-09-04 12:39:35 +0200

    trying css

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
-  $(function(){
-    $("#nav-placeholder").load("/static/nav.html");
+  // Load the navigation bar into the placeholder div
+  $("#navbar-placeholder").load("/static/nav.html", function() {
+      // Remove 'active' class from any currently active element
+      $("#navbar-placeholder nav a").removeClass("active");
+
+      // Get the current URL path
+      var currentPath = window.location.pathname;
+
+      // Highlight based on the current path
+      if (currentPath.includes("/static/posts/posts.html")) {
+          // Add 'active' class to the Posts link
+          $("#navbar-placeholder #post").addClass("active");
+      } else if (currentPath.includes("/index.html")) {
+          // Add 'active' class to the Home link
+          $("#navbar-placeholder #home").addClass("active");
+      }
+
+      // Add more conditions if needed for other pages
   });
 });
